5ddb0f490d537bffe49380545a535f1de79c1ccd
Luke Soguero <lukesoguero@gmail.com>
2020-03-17 17:53:03 -0600

    Simple bayes model in test.py, more advanced model in notebook - not finished yet

--- test.py
+++ test.py
@@ -1,35 +1,33 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+from nltk.corpus import stopwords
+from sklearn.naive_bayes import MultinomialNB
+import csv
+
 
 train_df = pd.read_csv("train.csv")
 test_df = pd.read_csv("test.csv")
 
 count_vectorizer = feature_extraction.text.CountVectorizer()
 
-## let's get counts for the first 5 tweets in the data
-example_train_vectors = count_vectorizer.fit_transform(train_df["text"][0:5])
-
 train_vectors = count_vectorizer.fit_transform(train_df["text"])
 
-## note that we're NOT using .fit_transform() here. Using just .transform() makes sure
-# that the tokens in the train vectors are the only ones mapped to the test vectors - 
-# i.e. that the train and test vectors use the same set of tokens.
 test_vectors = count_vectorizer.transform(test_df["text"])
 
-## Our vectors are really big, so we want to push our model's weights
-## toward 0 without completely discounting different words - ridge regression 
-## is a good way to do this.
-clf = linear_model.RidgeClassifier()
-clf.fit(train_vectors, train_df["target"])
+clf = MultinomialNB()
+clf.fit(train_vectors, train_df["target"]);
 
 sample_submission = pd.read_csv("sample_submission.csv")
 sample_submission["target"] = clf.predict(test_vectors)
 sample_submission.head()
+
+with open('submission.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["id", "target"])
+    for i in range(len(test_df.index)):
+        row = [test_df["id"][i], clf.predict(test_vectors[i])[0]]
+        writer.writerow(row)
 
 scores = model_selection.cross_val_score(clf, train_vectors, train_df["target"], cv=3, scoring="f1")
 print(scores)
